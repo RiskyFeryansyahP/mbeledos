56dcd804c8d146467c0e05308d264586ccb1c64e
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2019-12-09 21:54:26 +0700

    added name user after login

--- app/src/main/java/com/example/bledos/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/bledos/Fragments/HomeFragment.java
@@ -9,14 +9,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.bledos.Helper.SharedPreferencesConfig;
 import com.example.bledos.R;
 
 public class HomeFragment extends Fragment {
 
+    private SharedPreferencesConfig sharedPreferencesConfig;
+
+    private TextView txtWelcomeHome;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        sharedPreferencesConfig = new SharedPreferencesConfig(getActivity());
+
         return inflater.inflate(R.layout.activity_home_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        String name = sharedPreferencesConfig.readNameProfile();
+
+
+        txtWelcomeHome = getActivity().findViewById(R.id.txtWelcomeHome);
+        txtWelcomeHome.setText("Hello, " + name);
     }
 }
