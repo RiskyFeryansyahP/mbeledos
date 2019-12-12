a1972cf058acc107d013494c64467fff87200d30
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2019-12-12 22:50:42 +0700

    added new field for model sign up

--- app/src/main/java/com/example/bledos/model/SignUpModel.java
+++ app/src/main/java/com/example/bledos/model/SignUpModel.java
@@ -10,10 +10,19 @@ public class SignUpModel {
 
     private String nohp;
 
-    public SignUpModel(String nama, String alamat, String tgl_lahir, String nohp) {
+    private int level;
+
+    private String image;
+
+    private String kategori_level;
+
+    public SignUpModel(String nama, String alamat, String tgl_lahir, String nohp, int level, String image, String kategori_level) {
         this.nama = nama;
         this.alamat = alamat;
         this.tgl_lahir = tgl_lahir;
         this.nohp = nohp;
+        this.level = level;
+        this.image = image;
+        this.kategori_level = kategori_level;
     }
 }
