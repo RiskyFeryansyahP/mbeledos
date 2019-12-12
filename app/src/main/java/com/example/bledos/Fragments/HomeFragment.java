e58f749dc7b167b205481d90c6aef6b146c5e511
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2019-12-12 22:49:54 +0700

    added call set adapter for recycler view home

--- app/src/main/java/com/example/bledos/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/bledos/Fragments/HomeFragment.java
@@ -2,19 +2,25 @@ package com.example.bledos.Fragments;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.bledos.Adapter.RecycleViewAdapterHome;
 import com.example.bledos.Helper.SharedPreferencesConfig;
 import com.example.bledos.R;
+import com.google.gson.JsonObject;
 
 public class HomeFragment extends Fragment {
+
+    private static final String TAG = "HomeFragment";
 
     private SharedPreferencesConfig sharedPreferencesConfig;
 
@@ -32,10 +38,25 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        String name = sharedPreferencesConfig.readNameProfile();
-
-
         txtWelcomeHome = getActivity().findViewById(R.id.txtWelcomeHome);
-        txtWelcomeHome.setText("Hello, " + name);
+
+        JsonObject profile = sharedPreferencesConfig.readUserProfile();
+
+        Log.d(TAG, "onViewCreated: " + profile.get("nama").toString());
+
+        txtWelcomeHome.setText("Hello, " + profile.get("nama").getAsString());
+
+        setAdapterRecyclerViewHome();
+    }
+
+    public void setAdapterRecyclerViewHome() {
+        Log.d(TAG, "setAdapterRecyclerViewHome: Called");
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        RecyclerView recyclerView = getActivity().findViewById(R.id.recyclerViewHome);
+        recyclerView.setLayoutManager(layoutManager);
+
+        RecycleViewAdapterHome recycleViewAdapterHome = new RecycleViewAdapterHome();
+        recyclerView.setAdapter(recycleViewAdapterHome);
     }
 }
