f772da8a27efd257c6d350d5d9eb2f8c30532a01
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2019-12-13 14:11:38 +0700

    called api to backend

--- app/src/main/java/com/example/bledos/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/bledos/Fragments/HomeFragment.java
@@ -16,7 +16,18 @@ import android.widget.TextView;
 import com.example.bledos.Adapter.RecycleViewAdapterHome;
 import com.example.bledos.Helper.SharedPreferencesConfig;
 import com.example.bledos.R;
+import com.example.bledos.interfaces.BengkelAPI;
+import com.example.bledos.model.BengkelModel;
 import com.google.gson.JsonObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class HomeFragment extends Fragment {
 
@@ -25,6 +36,13 @@ public class HomeFragment extends Fragment {
     private SharedPreferencesConfig sharedPreferencesConfig;
 
     private TextView txtWelcomeHome;
+
+    private BengkelAPI bengkelAPI;
+
+    // data for recyclerview
+    private ArrayList<String> kodeBengkel = new ArrayList<>();
+    private ArrayList<String> namaBengkel = new ArrayList<>();
+    private ArrayList<String> alamatBengkel = new ArrayList<>();
 
     @Nullable
     @Override
@@ -42,21 +60,62 @@ public class HomeFragment extends Fragment {
 
         JsonObject profile = sharedPreferencesConfig.readUserProfile();
 
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://"+getActivity().getResources().getString(R.string.ip_server)+":8080/bengkel/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        bengkelAPI = retrofit.create(BengkelAPI.class);
+
         Log.d(TAG, "onViewCreated: " + profile.get("nama").toString());
 
         txtWelcomeHome.setText("Hello, " + profile.get("nama").getAsString());
 
-        setAdapterRecyclerViewHome();
+        GetAllBengkel();
     }
 
-    public void setAdapterRecyclerViewHome() {
+    public void GetAllBengkel() {
+        Call<List<BengkelModel>> call = bengkelAPI.GetAllBengkel();
+        call.enqueue(new Callback<List<BengkelModel>>() {
+            @Override
+            public void onResponse(Call<List<BengkelModel>> call, Response<List<BengkelModel>> response) {
+                if (!response.isSuccessful()) {
+                    Log.d(TAG, "onResponse: Failed : " + response.code());
+                    return;
+                }
+
+                if (response.body() != null) {
+                    List<BengkelModel> bengkels = response.body();
+
+                    for (BengkelModel bengkel : bengkels) {
+                        kodeBengkel.add(bengkel.getKode_bengkel());
+                        namaBengkel.add(bengkel.getNama_bengkel());
+                        alamatBengkel.add(bengkel.getAlamat_bengkel());
+                    }
+
+                    setAdapterRecyclerViewHome(kodeBengkel, namaBengkel, alamatBengkel);
+                }
+                else
+                {
+                    Log.d(TAG, "onResponse: Data is Null");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<BengkelModel>> call, Throwable t) {
+                Log.e(TAG, "onFailure: " + t.getMessage() );
+            }
+        });
+    }
+
+    public void setAdapterRecyclerViewHome(ArrayList<String> kodeBengkel, ArrayList<String> namaBengkel, ArrayList<String> alamatBengkel) {
         Log.d(TAG, "setAdapterRecyclerViewHome: Called");
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
         RecyclerView recyclerView = getActivity().findViewById(R.id.recyclerViewHome);
         recyclerView.setLayoutManager(layoutManager);
 
-        RecycleViewAdapterHome recycleViewAdapterHome = new RecycleViewAdapterHome();
+        RecycleViewAdapterHome recycleViewAdapterHome = new RecycleViewAdapterHome(kodeBengkel, namaBengkel, alamatBengkel);
         recyclerView.setAdapter(recycleViewAdapterHome);
     }
 }
