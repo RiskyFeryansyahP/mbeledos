004a922eaf8664a751af41013d626ed7b5391cde
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2019-12-13 14:11:59 +0700

    added model of Bengkel

---
+++ app/src/main/java/com/example/bledos/model/BengkelModel.java
@@ -0,0 +1,34 @@
+package com.example.bledos.model;
+
+public class BengkelModel {
+
+    private String kode_bengkel;
+
+    private String nama_bengkel;
+
+    private String alamat_bengkel;
+
+    private double latitude;
+
+    private double longitude;
+
+    public String getKode_bengkel() {
+        return kode_bengkel;
+    }
+
+    public String getNama_bengkel() {
+        return nama_bengkel;
+    }
+
+    public String getAlamat_bengkel() {
+        return alamat_bengkel;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+}
