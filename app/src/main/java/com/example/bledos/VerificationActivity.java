3a79ccd345c0b3cc3eba4040f24f5ebe658d7a4b
Shania Saraswati <=>
2019-12-14 21:12:08 +0700

    add verification activity

---
+++ app/src/main/java/com/example/bledos/VerificationActivity.java
@@ -0,0 +1,16 @@
+package com.example.bledos;
+
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class VerificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verification);
+    }
+}
