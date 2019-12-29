a158e8fc4152af781dec0d517aa248beab14eddd
Trio Kurniawan <171111042@mhs.stiki.ac.id>
2019-12-29 21:07:58 +0700

    added logic in order

--- app/src/main/java/com/example/bledos/OrderActivity.java
+++ app/src/main/java/com/example/bledos/OrderActivity.java
@@ -2,13 +2,15 @@ package com.example.bledos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity implements View.OnClickListener {
-
+    Button imgtombolpesan;
     ImageButton imgButtonPlusBanBocor, imgButtonPlusBanDalam, imgButtonPlusBanLuar;
     ImageButton imgButtonMenBanBocor, imgButtonMenBanDalam, imgButtonMenBanLuar;
     TextView txtBanBocor, txtBanLuar, txtBanDalam;
@@ -17,6 +19,9 @@ public class OrderActivity extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        imgtombolpesan = findViewById(R.id.tombolpesan);
+        imgtombolpesan.setOnClickListener(this);
 
         imgButtonPlusBanBocor = findViewById(R.id.imgButtonPlusBanBocor);
         imgButtonPlusBanDalam = findViewById(R.id.imgButtonPlusBanDalam);
@@ -33,6 +38,10 @@ public class OrderActivity extends AppCompatActivity implements View.OnClickList
         imgButtonPlusBanDalam.setOnClickListener(this);
         imgButtonPlusBanLuar.setOnClickListener(this);
 
+        imgButtonMenBanDalam.setEnabled(false);
+        imgButtonMenBanBocor.setEnabled(false);
+        imgButtonMenBanLuar.setEnabled(false);
+
         imgButtonMenBanBocor.setOnClickListener(this);
         imgButtonMenBanDalam.setOnClickListener(this);
         imgButtonMenBanLuar.setOnClickListener(this);
@@ -43,33 +52,43 @@ public class OrderActivity extends AppCompatActivity implements View.OnClickList
         switch (v.getId()) {
             case R.id.imgButtonPlusBanBocor:
                 txtBanBocor.setText("1");
+                TransaksiActivity.BanBocor = 1;
                 imgButtonPlusBanBocor.setEnabled(false);
                 imgButtonMenBanBocor.setEnabled(true);
                 break;
             case R.id.imgButtonPlusBanDalam:
                 txtBanDalam.setText("1");
+                TransaksiActivity.BanDalam = 1;
                 imgButtonPlusBanDalam.setEnabled(false);
                 imgButtonMenBanDalam.setEnabled(true);
                 break;
             case R.id.imgButtonPlusBanLuar:
                 txtBanLuar.setText("1");
+                TransaksiActivity.BanLuar = 1;
                 imgButtonPlusBanLuar.setEnabled(false);
                 imgButtonMenBanLuar.setEnabled(true);
                 break;
             case R.id.imgButtonMenBanBocor:
                 txtBanBocor.setText("0");
+                TransaksiActivity.BanBocor= 0;
                 imgButtonMenBanBocor.setEnabled(false);
                 imgButtonPlusBanBocor.setEnabled(true);
                 break;
             case R.id.imgButtonMenBanDalam:
                 txtBanDalam.setText("0");
+                TransaksiActivity.BanDalam = 0;
                 imgButtonMenBanDalam.setEnabled(false);
                 imgButtonPlusBanDalam.setEnabled(true);
                 break;
             case R.id.imgButtonMenBanLuar:
                 txtBanLuar.setText("0");
+                TransaksiActivity.BanLuar = 0;
                 imgButtonMenBanLuar.setEnabled(false);
                 imgButtonPlusBanLuar.setEnabled(true);
+                break;
+
+            case R.id.tombolpesan:
+                startActivity(new Intent(OrderActivity.this,TransaksiActivity.class));
                 break;
         }
     }
