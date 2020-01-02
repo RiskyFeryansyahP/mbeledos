e2bbaa4fced3fe8423a093a89b416b44f061b908
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-01-02 16:29:56 +0700

    added enpoint for get nearest bengkel

--- app/src/main/java/com/example/bledos/interfaces/BengkelAPI.java
+++ app/src/main/java/com/example/bledos/interfaces/BengkelAPI.java
@@ -1,17 +1,23 @@
 package com.example.bledos.interfaces;
 
 import com.example.bledos.model.BengkelModel;
+import com.example.bledos.model.LocationModel;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface BengkelAPI {
 
     @GET("all")
     Call<List<BengkelModel>> GetAllBengkel();
+
+    @POST("nearest")
+    Call<List<BengkelModel>> GetNearestBengkel(@Body LocationModel locationModel);
 
     @GET("kode/{kode_bengkel}")
     Call<BengkelModel> GetDetailBengkel(@Path("kode_bengkel") String kode_bengkel);
