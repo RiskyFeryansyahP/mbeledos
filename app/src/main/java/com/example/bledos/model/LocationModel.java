fa5a793f27f5e87fbd3d5f69c40be052b7b1a667
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-01-02 16:30:13 +0700

    add model to send location

---
+++ app/src/main/java/com/example/bledos/model/LocationModel.java
@@ -0,0 +1,13 @@
+package com.example.bledos.model;
+
+public class LocationModel {
+
+    private double latitude;
+
+    private double longitude;
+
+    public LocationModel(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
