ec76f888957c89c4572e74f03fea6be20f83a379
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-01-03 15:38:24 +0700

    added redirect to maps order

--- app/src/main/java/com/example/bledos/TransaksiActivity.java
+++ app/src/main/java/com/example/bledos/TransaksiActivity.java
@@ -2,43 +2,56 @@ package com.example.bledos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class TransaksiActivity extends AppCompatActivity {
 
     private static final String TAG = "TransaksiActivity";
 
-    public static int BanBocor;
-    public static int BanDalam;
-    public static int BanLuar;
-    public static int TotalHarga;
+    public static int JumlahBanBocor;
+    public static int JumlahBanDalam;
+    public static int JumlahBanLuar;
 
-    TextView HargaBanBocor, HargaBanDalam, HargaBanLuar, Total;
+
+    private int TotalHarga, TotalBanBocor = 0, TotalBanDalam = 0, TotalBanLuar = 0;
+
+    private TextView HargaBanBocor, HargaBanDalam, HargaBanLuar, Total;
+
+    private Button tombolPesanan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaksi);
 
-        BanBocor = BanBocor * 15000;
-        BanDalam = BanDalam * 125000;
-        BanLuar = BanLuar * 30000;
-
-        TotalHarga = BanBocor + BanDalam + BanLuar;
-
         HargaBanBocor = findViewById(R.id.HargaBanBocor);
         HargaBanDalam = findViewById(R.id.HargaBanDalam);
         HargaBanLuar = findViewById(R.id.HargaBanLuar);
         Total = findViewById(R.id.Total);
 
-        HargaBanBocor.setText("Rp. " + String.valueOf(BanBocor));
-        HargaBanDalam.setText("Rp" +String.valueOf(BanDalam) );
-        HargaBanLuar.setText("Rp" +String.valueOf(BanLuar));
+        tombolPesanan = findViewById(R.id.tombolpesan);
+
+        TotalBanBocor = JumlahBanBocor * 15000;
+        TotalBanDalam = JumlahBanDalam * 125000;
+        TotalBanLuar = JumlahBanLuar * 30000;
+
+        TotalHarga = TotalBanBocor + TotalBanDalam + TotalBanLuar;
+
+        HargaBanBocor.setText("Rp. " + String.valueOf(TotalBanBocor));
+        HargaBanDalam.setText("Rp" +String.valueOf(TotalBanDalam) );
+        HargaBanLuar.setText("Rp" +String.valueOf(TotalBanLuar));
         Total.setText("Rp"  +  String.valueOf(TotalHarga));
 
-
+        tombolPesanan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(TransaksiActivity.this, MapsOrder.class));
+            }
+        });
 
     }
 }
