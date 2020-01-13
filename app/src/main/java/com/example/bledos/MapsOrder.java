a53512bf805b7181c5fda9a70c850de554882a61
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-01-13 11:52:49 +0700

    added direction two point origin and destination

--- app/src/main/java/com/example/bledos/MapsOrder.java
+++ app/src/main/java/com/example/bledos/MapsOrder.java
@@ -3,9 +3,21 @@ package com.example.bledos;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.bledos.Helper.SharedPreferencesConfig;
+import com.mapbox.api.directions.v5.DirectionsCriteria;
+import com.mapbox.api.directions.v5.MapboxDirections;
+import com.mapbox.api.directions.v5.models.DirectionsResponse;
+import com.mapbox.api.directions.v5.models.DirectionsRoute;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.LineString;
+import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
@@ -15,16 +27,46 @@ import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.style.layers.LineLayer;
+import com.mapbox.mapboxsdk.style.layers.Property;
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+import com.mapbox.mapboxsdk.utils.BitmapUtils;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static com.mapbox.core.constants.Constants.PRECISION_6;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAllowOverlap;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconIgnorePlacement;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconOffset;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineCap;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineColor;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineJoin;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineWidth;
 
 public class MapsOrder extends AppCompatActivity {
 
     private static final String TAG = "MapsOrder";
 
+    private static final String ROUTE_LAYER_ID = "route-layer-id";
+    private static final String ROUTE_SOURCE_ID = "route-source-id";
+    private static final String ICON_LAYER_ID = "icon-layer-id";
+    private static final String ICON_SOURCE_ID = "icon-source-id";
+    private static final String RED_PIN_ICON_ID = "red-pin-icon-id";
+
     private MapView mapView;
+    private DirectionsRoute directionsRoute;
+    private MapboxDirections client;
 
     private SharedPreferencesConfig sharedPreferencesConfig;
 
     private double Latitude, Longitude;
+    private double LatitudePlace, LongitudePlace;
+
+    private Point origin, destination;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,22 +77,33 @@ public class MapsOrder extends AppCompatActivity {
         sharedPreferencesConfig = new SharedPreferencesConfig(this);
         Latitude = sharedPreferencesConfig.readLatitudeLocation();
         Longitude = sharedPreferencesConfig.readLongitudeLocation();
+        LatitudePlace = OrderActivity.latitudePlace;
+        LongitudePlace = OrderActivity.longitudePlace;
 
         mapView = findViewById(R.id.mapView);
 
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
-            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+            public void onMapReady(@NonNull final MapboxMap mapboxMap) {
                 mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
                     @Override
                     public void onStyleLoaded(@NonNull Style style) {
 
+                            origin = Point.fromLngLat(Longitude, Latitude);
+                            destination = Point.fromLngLat(LongitudePlace, LatitudePlace);
+
+                            initSource(style);
+
+                            initLayers(style);
+
+                            getRoute(mapboxMap, origin, destination);
+
+                        mapboxMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(Latitude, Longitude), 15.0));
+                        mapboxMap.addMarker(new MarkerOptions().position(new LatLng(Latitude, Longitude)).setTitle("Origin"));
+                        mapboxMap.addMarker(new MarkerOptions().position(new LatLng(LatitudePlace, LongitudePlace)).setTitle("Destination"));
                     }
                 });
-                mapboxMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(Latitude, Longitude), 15.0));
-                mapboxMap.addMarker(new MarkerOptions().position(new LatLng(Latitude, Longitude)).setTitle("A"));
-
             }
         });
     }
@@ -95,5 +148,102 @@ public class MapsOrder extends AppCompatActivity {
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
+    }
+
+    /*
+    * Add the route and marker sources to the map
+     */
+    private void initSource(Style style) {
+        style.addSource(new GeoJsonSource(ROUTE_SOURCE_ID, FeatureCollection.fromFeatures(new Feature[] {})));
+
+        GeoJsonSource iconGeoJsonSource = new GeoJsonSource(ICON_SOURCE_ID, FeatureCollection.fromFeatures(new Feature[]{
+                Feature.fromGeometry(Point.fromLngLat(origin.longitude(), origin.latitude())),
+                Feature.fromGeometry(Point.fromLngLat(destination.longitude(), destination.latitude()))
+        }));
+        style.addSource(iconGeoJsonSource);
+    }
+
+    /*
+    * Add the route and marker icon layers to the map
+     */
+    private void initLayers(Style style) {
+        LineLayer lineLayer = new LineLayer(ROUTE_LAYER_ID, ROUTE_SOURCE_ID);
+
+        // Add the LineLayer to the map. This layer will display the directions route.
+        lineLayer.setProperties(
+                lineCap(Property.LINE_CAP_ROUND),
+                lineJoin(Property.LINE_JOIN_ROUND),
+                lineWidth(5f),
+                lineColor(Color.parseColor("#009688"))
+        );
+
+        style.addLayer(lineLayer);
+
+        // Add the red marker icon image to the map
+//        style.addImage(RED_PIN_ICON_ID, BitmapUtils.getBitmapFromDrawable(getResources().getDrawable(R.drawable.red_marker)));
+
+        // Add the red marker icon SymbolLayer to the map
+        style.addLayer(new SymbolLayer(ICON_LAYER_ID, ICON_SOURCE_ID).withProperties(
+                iconImage(RED_PIN_ICON_ID),
+                iconIgnorePlacement(true),
+                iconAllowOverlap(true),
+                iconOffset(new Float[]{0f, -9f})
+        ));
+    }
+
+    private void getRoute(final MapboxMap mapboxMap, Point origin, Point destination) {
+        client = MapboxDirections.builder()
+                .origin(origin)
+                .destination(destination)
+                .overview(DirectionsCriteria.OVERVIEW_FULL)
+                .profile(DirectionsCriteria.PROFILE_DRIVING)
+                .accessToken(getResources().getString(R.string.API_KEY_MAPBOX))
+                .build();
+
+        client.enqueueCall(new Callback<DirectionsResponse>() {
+            @SuppressLint("StringFormatInvalid")
+            @Override
+            public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
+                if (!response.isSuccessful()) {
+                    Log.d(TAG, "onResponse: unSeccessful " + response.code());
+                }
+
+                if (response.body() == null) {
+                    Log.d(TAG, "response body null ");
+                    return;
+                } else if (response.body().routes().size() < 1) {
+                    Log.d(TAG, "No Routes Found");
+                    return;
+                }
+
+                directionsRoute = response.body().routes().get(0);
+
+                Toast.makeText(MapsOrder.this, String.format(
+                    getString(R.string.directions_activity_toast_message),
+                    directionsRoute.distance()), Toast.LENGTH_SHORT).show();
+
+                if (mapboxMap != null) {
+                    mapboxMap.getStyle(new Style.OnStyleLoaded() {
+                        @Override
+                        public void onStyleLoaded(@NonNull Style style) {
+                            // Retrieve and update the source designated for showing the directions route
+                            GeoJsonSource source = style.getSourceAs(ROUTE_SOURCE_ID);
+
+                            // Create a LineString with the directions route's geometry and
+                            // reset the GeoJSON source for the route LineLayer source
+                            if (source != null) {
+                                Log.d(TAG, "onStyleLoaded: Source != null");
+                                source.setGeoJson(FeatureCollection.fromFeature(Feature.fromGeometry(LineString.fromPolyline(directionsRoute.geometry(), PRECISION_6))));
+                            }
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onFailure(Call<DirectionsResponse> call, Throwable t) {
+                Log.e(TAG, "onFailure: " + t.getMessage() );
+            }
+        });
     }
 }
