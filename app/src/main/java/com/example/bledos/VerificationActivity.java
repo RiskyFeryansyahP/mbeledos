704e4048e84e17a6f733b3f006470aa9de571dbb
Shania Saraswati <=>
2020-01-14 15:52:23 +0700

    added text watcher

--- app/src/main/java/com/example/bledos/VerificationActivity.java
+++ app/src/main/java/com/example/bledos/VerificationActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -40,6 +42,11 @@ public class VerificationActivity extends AppCompatActivity {
         code4 = findViewById(R.id.code4);
         button = findViewById(R.id.btnsubmit);
 
+        code1.addTextChangedListener(VerificationTextWatcher);
+        code2.addTextChangedListener(VerificationTextWatcher);
+        code3.addTextChangedListener(VerificationTextWatcher);
+        code4.addTextChangedListener(VerificationTextWatcher);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -49,6 +56,8 @@ public class VerificationActivity extends AppCompatActivity {
                 String kode3 = code3.getText().toString();
                 String kode4 = code4.getText().toString();
 
+
+
                 String kode = kode1 + "" + kode2 + "" + kode3 + "" + kode4;
 
                 code = Integer.parseInt(kode);
@@ -57,7 +66,68 @@ public class VerificationActivity extends AppCompatActivity {
                     sharedPreferencesConfig.writeLoginStatus(true);
                     startActivity(new Intent(getApplicationContext(), HomeActivity.class));
                 }
+
+
             }
         });
     }
+    private TextWatcher VerificationTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            if (editable == code1.getEditableText())
+            {
+                if(code1.length() == 1)
+                {
+                    code2.requestFocus();
+                }
+                else if(code1.length() == 0)
+                {
+                    code1.clearFocus();
+                }
+            }
+            else if(editable == code2.getEditableText())
+            {
+                if(code2.length() == 1)
+                {
+                    code3.requestFocus();
+                }
+                else if(code2.length() == 0)
+                {
+                    code1.requestFocus();
+                }
+            }
+            else if(editable == code3.getEditableText())
+            {
+                if(code3.length() == 1)
+                {
+                    code4.requestFocus();
+                }
+                else if(code3.length() == 0)
+                {
+                    code2.requestFocus();
+                }
+            }
+            else if(editable == code4.getEditableText())
+            {
+                if(code4.length() == 1)
+                {
+                    code4.clearFocus();
+                }
+                else if(code4.length() == 0)
+                {
+                    code3.requestFocus();
+                }
+            }
+        }
+    };
 }
